779a65633ae9768cabea19bef902786f4e7a7de1
FrGery <f2gergo@gmail.com>
2026-01-05 00:38:28 +0100

    Add client hydration support to server configuration

--- angular-frontend/src/app/app.config.server.ts
+++ angular-frontend/src/app/app.config.server.ts
@@ -1,13 +1,14 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server'; // Javítva az import
-import { provideNoopAnimations } from '@angular/platform-browser/animations'; // Ezt add hozzá!
-import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import {ApplicationConfig, mergeApplicationConfig} from '@angular/core';
+import {provideServerRendering} from '@angular/platform-server'; // Javítva az import
+import {provideNoopAnimations} from '@angular/platform-browser/animations'; // Ezt add hozzá!
+import {appConfig} from './app.config';
+import { provideClientHydration } from '@angular/platform-browser'; // Ezt add hozzá
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideNoopAnimations() // Ez hatástalanítja a hibaüzenetet a build során
+    provideNoopAnimations(),
+    provideClientHydration()
   ]
 };
 
